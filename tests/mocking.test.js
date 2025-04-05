ba1e93e9e70a9698616ff3600c3679664020b029
Samsegun <oyebadesegunsam@gmail.com>
2025-04-05 13:21:32 +0100

    spy functions

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,19 +1,33 @@
-import { vi, it, expect, describe } from "vitest";
+import { vi, it, expect, describe, beforeEach } from "vitest";
 import {
+    getDiscount,
     getPriceInCurrency,
     getShippingInfo,
+    isOnline,
+    login,
     renderPage,
+    signUp,
     submitOrder,
 } from "../src/mocking";
 import { getExchangeRate } from "../src/libs/currency";
 import { getShippingQuote } from "../src/libs/shipping";
 import { trackPageView } from "../src/libs/analytics";
 import { charge } from "../src/libs/payment";
+import { sendEmail } from "../src/libs/email";
+import security from "../src/libs/security";
 
 vi.mock("../src/libs/currency");
 vi.mock("../src/libs/shipping");
 vi.mock("../src/libs/analytics");
 vi.mock("../src/libs/payment");
+vi.mock("../src/libs/email", async importOriginal => {
+    const originalModule = await importOriginal();
+
+    return {
+        ...originalModule,
+        sendEmail: vi.fn(),
+    };
+});
 
 describe("test suite", () => {
     it("test case", () => {
@@ -115,5 +129,84 @@ describe("submitOrder", () => {
         const result = await submitOrder(order, creditCard);
 
         expect(result).toEqual({ success: true });
+    });
+});
+
+describe("signUp", () => {
+    const email = "name@domain.com";
+
+    // beforeEach(() => {
+    //     vi.mocked(sendEmail).mockClear();
+    // });
+
+    it("should return false if email is not valid", async () => {
+        const result = await signUp("part");
+
+        expect(result).toBe(false);
+    });
+
+    it("should return true if email is valid", async () => {
+        const result = await signUp(email);
+
+        expect(result).toBe(true);
+    });
+
+    it("should send the welcome email if email is valid", async () => {
+        const result = await signUp(email);
+
+        expect(sendEmail).toHaveBeenCalledOnce();
+
+        const args = vi.mocked(sendEmail).mock.calls[0];
+        expect(args[0]).toBe(email);
+        expect(args[1]).toMatch(/welcome/i);
+    });
+});
+
+describe("login", () => {
+    it("should email the one-time login code", async () => {
+        const email = "name@domain.com";
+
+        const spy = vi.spyOn(security, "generateCode");
+
+        await login(email);
+
+        const securityCode = spy.mock.results[0].value.toString();
+        expect(sendEmail).toHaveBeenCalledWith(email, securityCode);
+    });
+});
+
+describe("isOnline", () => {
+    it("should return false if currentHour is outside opening hours", () => {
+        vi.setSystemTime("2025-04-04 07:59");
+        expect(isOnline()).toBe(false);
+
+        vi.setSystemTime("2025-04-04 20:01");
+        expect(isOnline()).toBe(false);
+    });
+
+    it("should return true if currentHour is within opening hours", () => {
+        vi.setSystemTime("2025-04-04 08:00");
+        expect(isOnline()).toBe(true);
+
+        vi.setSystemTime("2025-04-04 19:59");
+        expect(isOnline()).toBe(true);
+    });
+});
+
+describe("getDiscount", () => {
+    it("should return 0.2 discount if it is christmas day", () => {
+        vi.setSystemTime("2025-12-25 00:01");
+        expect(getDiscount()).toBe(0.2);
+
+        vi.setSystemTime("2025-12-25 23:59");
+        expect(getDiscount()).toBe(0.2);
+    });
+
+    it("should return 0 discount if it is not christmas day", () => {
+        vi.setSystemTime("2025-12-24 23:59");
+        expect(getDiscount()).toBe(0);
+
+        vi.setSystemTime("2025-12-26 00:01");
+        expect(getDiscount()).toBe(0);
     });
 });
